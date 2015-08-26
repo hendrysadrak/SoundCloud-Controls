c2b1376e79b76490e2a423f482ae705ba18e8d3f
Hendry Sadrak <hendry.sadrak@outlook.com>
2015-08-27 01:21:50 +0300

    First Commit

---
+++ js/popup.js
@@ -0,0 +1,105 @@
+/**
+ * main code
+ */
+(function () {
+    /**
+     * trackMethod
+     * @type {string}
+     */
+    var trackMethod;
+
+    function onControlsClick(e) {
+        /**
+         * Set the trackMethod
+         */
+        trackMethod = $(this).data('type') || e;
+
+        /**
+         * Get soundcloud tabs
+         */
+        chrome.tabs.query({
+            currentWindow: true,
+            url: "*://soundcloud.com/*"
+        }, runScriptInTabs);
+    }
+
+    function runScriptInTabs(tabs) {
+        for (var key in tabs) {
+            var tab = tabs[key];
+
+            runScriptInTab(tab);
+        }
+    }
+
+    function runScriptInTab(tab) {
+        var tabId = tab.id;
+
+        chrome.tabs.sendMessage(tabId, {
+            trackMethod: trackMethod
+        }, setBtnIsPlaying);
+    }
+
+    function setBtnIsPlaying(c) {
+        if (c.isPlaying) {
+            $('.btn-pause')
+                .attr('title', 'Pause')
+                .removeClass('is-play');
+        }
+        else {
+            $('.btn-pause')
+                .attr('title', 'Play')
+                .addClass('is-play');
+        }
+    };
+
+    function main() {
+        $('.btn-control').on('click', onControlsClick);
+        $('.js-settings').on('click', function (e) {
+            e.preventDefault();
+        });
+    }
+
+    /**
+     * Start the party!
+     */
+    main();
+
+    /**
+     * Send fake call
+     */
+    onControlsClick('fake');
+})();
+
+/**
+ * ripple effect
+ */
+$(document).on('click', '.ripple', function (e) {
+    var $rippleElement = $('<span class="ripple-effect" />'),
+        $buttonElement = $(this),
+        btnOffset = $buttonElement.offset(),
+        xPos = e.pageX - btnOffset.left,
+        yPos = e.pageY - btnOffset.top,
+        size = parseInt(Math.min($buttonElement.height(), $buttonElement.width()) * 0.5),
+        animateSize = parseInt(Math.max($buttonElement.width(), $buttonElement.height()) * Math.PI);
+
+    $rippleElement
+        .css({
+            top: yPos,
+            left: xPos,
+            width: size,
+            height: size,
+
+            backgroundColor: $buttonElement.data("ripple-color")
+        })
+        .appendTo($buttonElement)
+        .animate({
+            width: animateSize,
+            height: animateSize,
+            opacity: 0
+        }, {
+            duration: 500,
+            complete: function () {
+                $(this).remove();
+            }
+        });
+});
---
+++ js/runInTab.js
@@ -0,0 +1,46 @@
+(function () {
+    "use strict";
+
+    if (window.hasHSInject) return;
+    else window.hasHSInject = true;
+
+    var methodObject = {
+        next: function () {
+            clickButtonElement(getButtonElement('skipControl__next'));
+        },
+        pause: function () {
+            clickButtonElement(getButtonElement('playControl'));
+        },
+        prev: function () {
+            clickButtonElement(getButtonElement('skipControl__previous'));
+        }
+    };
+
+    function getButtonElement(className) {
+        return document.querySelector('.playControls__playPauseSkip .' + className);
+    }
+
+    function clickButtonElement($button) {
+        $button.click();
+    }
+
+    function runMethod(trackMethod) {
+        methodObject[trackMethod]();
+    }
+
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+        /**
+         * do the running
+         */
+        if (message.trackMethod != 'fake')runMethod(message.trackMethod);
+
+        /**
+         * Send back if is playing
+         */
+        var isPlaying = getButtonElement('playControl').classList.contains('playing');
+
+        sendResponse({
+            isPlaying: isPlaying
+        });
+    });
+})();
