caf7b06d4addb60a24f3d26f04261d8d67d0fa6b
Hendry Sadrak <hendry.sadrak@outlook.com>
2017-03-29 10:46:36 +0300

    update to 1.2.0 - fixing comatability issues

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,106 +1,96 @@
 "use strict";
 
-var service = analytics.getService('hs.soundcloud_controls');
-var tracker = service.getTracker('UA-62812857-3');
+var service = analytics.getService( 'hs.soundcloud_controls' );
+var tracker = service.getTracker( 'UA-62812857-3' );
 
-tracker.sendAppView('MainView');
-tracker.sendEvent('View', 'Open', 'Main');
+tracker.sendAppView( 'MainView' );
+tracker.sendEvent( 'View', 'Open', 'Main' );
 
 /**
  * main code
  */
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener( 'DOMContentLoaded', function () {
     /**
      * trackMethod
      * @type {string}
      */
     var trackMethod;
 
-    function onControlsClick(e) {
+    function onControlsClick( e ) {
         /**
          * Set the trackMethod
          */
-        trackMethod = $(this).data('type') || e;
+        trackMethod = $( this ).data( 'type' ) || e;
 
         /**
          * send ga
          */
-        tracker.sendEvent('Controls', 'Click', trackMethod);
+        tracker.sendEvent( 'Controls', 'Click', trackMethod );
 
         /**
          * Get soundcloud tabs
          */
-        chrome.tabs.query({
+        chrome.tabs.query( {
             currentWindow: true,
-            url: "*://soundcloud.com/*"
-        }, runScriptInTabs);
+            url:           "*://soundcloud.com/*"
+        }, runScriptInTabs );
     }
 
     function checkForSoundCloud() {
-        chrome.tabs.query({
+        chrome.tabs.query( {
             currentWindow: true,
-            url: "*://soundcloud.com/*"
-        }, function (tabs) {
-            if (tabs.length > 0) $('.js-error-1').removeClass('show');
-            else $('.js-error-1').addClass('show');
-        });
+            url:           "*://soundcloud.com/*"
+        }, function ( tabs ) {
+            if ( tabs.length > 0 ) $( '.js-error-1' ).removeClass( 'show' );
+            else $( '.js-error-1' ).addClass( 'show' );
+        } );
     }
 
-    function runScriptInTabs(tabs) {
-        for (var key in tabs) {
-            var tab = tabs[key];
+    function runScriptInTabs( tabs ) {
+        for ( var key in tabs ) {
+            var tab = tabs[ key ];
 
-            runScriptInTab(tab);
+            runScriptInTab( tab );
         }
     }
 
-    function runScriptInTab(tab) {
+    function runScriptInTab( tab ) {
         var tabId = tab.id;
 
-        chrome.tabs.sendMessage(tabId, {
+        chrome.tabs.sendMessage( tabId, {
             trackMethod: trackMethod
-        }, afterScriptRun);
+        }, afterScriptRun );
     }
 
-    function afterScriptRun(c) {
+    function afterScriptRun( c ) {
         /**
          * Toggle play button
          */
-        if (c.isPlaying) {
-            $('.btn-pause')
-                .attr('title', 'Pause')
-                .removeClass('is-play');
+        if ( c.isPlaying ) {
+            $( '.btn-pause' )
+                .attr( 'title', 'Pause' )
+                .removeClass( 'is-play' );
         }
         else {
-            $('.btn-pause')
-                .attr('title', 'Play')
-                .addClass('is-play');
+            $( '.btn-pause' )
+                .attr( 'title', 'Play' )
+                .addClass( 'is-play' );
         }
 
         /**
          * Show song name
          */
-        $('.js-song')
-            .text(c.currentTrack || '')
-            .attr('title', c.currentTrack || '');
+        $( '.js-song' )
+            .text( c.currentTrack || '' )
+            .attr( 'title', c.currentTrack || '' );
 
-        tracker.sendEvent('Song', 'Listen', c.currentTrack);
+        tracker.sendEvent( 'Song', 'Listen', c.currentTrack );
     };
 
     function main() {
         checkForSoundCloud();
 
-        $('.btn-control').on('click', onControlsClick);
-
-        $('.js-settings').on('click', function (e) {
-            e.preventDefault();
-
-            tracker.sendEvent('Settings', 'Click', 'Open');
-        });
-
-        $('.js-author').on('click', function (e) {
-            tracker.sendEvent('Author', 'Click', 'Twitter');
-        });
+        $( '.btn-control' ).on( 'click', onControlsClick );
     }
 
     /**
@@ -111,38 +101,38 @@ document.addEventListener('DOMContentLoaded', function () {
     /**
      * Send fake call
      */
-    var callBack = onControlsClick('fake');
-});
+    var callBack = onControlsClick( 'fake' );
+} );
 
 /**
  * ripple effect
  */
-$(document).on('click', '.ripple', function (e) {
-    var $rippleElement = $('<span class="ripple-effect" />'),
-        $buttonElement = $(this),
-        btnOffset = $buttonElement.offset(),
-        xPos = e.pageX - btnOffset.left,
-        yPos = e.pageY - btnOffset.top,
-        size = parseInt(Math.min($buttonElement.height(), $buttonElement.width()) * 0.5),
-        animateSize = parseInt(Math.max($buttonElement.width(), $buttonElement.height()) * Math.PI);
+$( document ).on( 'click', '.ripple', function ( e ) {
+    var $rippleElement = $( '<span class="ripple-effect" />' ),
+        $buttonElement = $( this ),
+        btnOffset      = $buttonElement.offset(),
+        xPos           = e.pageX - btnOffset.left,
+        yPos           = e.pageY - btnOffset.top,
+        size           = parseInt( Math.min( $buttonElement.height(), $buttonElement.width() ) * 0.3 ),
+        animateSize    = parseInt( Math.max( $buttonElement.width(), $buttonElement.height() ) * Math.PI );
 
     $rippleElement
-        .css({
-            top: yPos,
-            left: xPos,
-            width: size,
-            height: size,
-            backgroundColor: $buttonElement.data("ripple-color")
-        })
-        .appendTo($buttonElement)
-        .animate({
-            width: animateSize,
-            height: animateSize,
+        .css( {
+            top:             yPos,
+            left:            xPos,
+            width:           size,
+            height:          size,
+            backgroundColor: $buttonElement.data( "ripple-color" )
+        } )
+        .appendTo( $buttonElement )
+        .animate( {
+            width:   animateSize,
+            height:  animateSize,
             opacity: 0
         }, {
             duration: 500,
             complete: function () {
-                $(this).remove();
+                $( this ).remove();
             }
-        });
-});
+        } );
+} );
--- src/js/runInTab.js
+++ src/js/runInTab.js
@@ -1,48 +1,48 @@
 (function () {
     "use strict";
 
-    if (window.hasHSInject) return;
+    if ( window.hasHSInject ) return;
     else window.hasHSInject = true;
 
     var methodObject = {
-        next: function () {
-            clickButtonElement(getButtonElement('skipControl__next'));
+        next:  function () {
+            clickButtonElement( getElement( '.playControls__next' ) );
         },
         pause: function () {
-            clickButtonElement(getButtonElement('playControl'));
+            clickButtonElement( getElement( '.playControls__play' ) );
         },
-        prev: function () {
-            clickButtonElement(getButtonElement('skipControl__previous'));
+        prev:  function () {
+            clickButtonElement( getElement( '.playControls__prev' ) );
         }
     };
 
-    function getButtonElement(className) {
-        return document.querySelector('.playControls__playPauseSkip .' + className);
+    function getElement( className ) {
+        return document.querySelector( '.playControls ' + className );
     }
 
-    function clickButtonElement($button) {
+    function clickButtonElement( $button ) {
         $button.click();
     }
 
-    function runMethod(trackMethod) {
-        methodObject[trackMethod]();
+    function runMethod( trackMethod ) {
+        methodObject[ trackMethod ]();
     }
 
-    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    chrome.runtime.onMessage.addListener( function ( message, sender, sendResponse ) {
         /**
          * do the running
          */
-        if (message.trackMethod != 'fake')runMethod(message.trackMethod);
+        if ( message.trackMethod != 'fake' ) runMethod( message.trackMethod );
 
         /**
          * Send back if is playing
          */
-        var isPlaying = getButtonElement('playControl').classList.contains('playing');
-        var currentTrack = document.getElementsByClassName('playbackSoundBadge__title')[0].title;
+        var isPlaying    = getElement( '.playControls__play' ).classList.contains( 'playing' );
+        var currentTrack = getElement( '.playbackSoundBadge__title' ).title;
 
-        sendResponse({
-            isPlaying: isPlaying,
+        sendResponse( {
+            isPlaying:    isPlaying,
             currentTrack: currentTrack
-        });
-    });
+        } );
+    } );
 })();
